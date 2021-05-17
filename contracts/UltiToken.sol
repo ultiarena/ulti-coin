c0b0386b52becc30bd4f01f0f0bdba9f899a90b2
harzo <harzojunior@gmail.com>
2021-05-17 14:38:13 +0200

    feat(contract): Add token release based on Pausable

--- contracts/UltiToken.sol
+++ contracts/UltiToken.sol
@@ -3,25 +3,34 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract UltiToken is ERC20, ERC20Capped, ERC20Burnable, AccessControl {
+contract UltiToken is ERC20, ERC20Capped, ERC20Burnable, Pausable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant RELEASE_ROLE = keccak256("RELEASE_ROLE");
 
     constructor() ERC20("UltiToken", "ULTI") ERC20Capped(150 * 1e9 * 1e18) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(RELEASE_ROLE, msg.sender);
+        _pause(); // Transfer and burn of the tokens are paused until the release
+    }
+
+    function release() onlyRole(RELEASE_ROLE) public whenPaused {
+        _unpause();
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
-        _burn(from, amount);
-    }
-
     function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
         ERC20Capped._mint(account, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
