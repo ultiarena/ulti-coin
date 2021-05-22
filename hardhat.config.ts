7b0dcc486ffb9a269845f3fb7d3547f6314538e2
harzo <harzojunior@gmail.com>
2021-05-22 21:11:50 +0200

    build: Migrate to Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+const { mnemonic } = require('./.secrets.json');
+
+module.exports = {
+  networks: {
+    bsc_testnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      chainId: 97,
+      accounts: {mnemonic: mnemonic}
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  solidity: {
+    version: '0.8.0',
+    settings: {
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: 'none',
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+}
