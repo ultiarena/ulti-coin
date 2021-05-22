15f385bef317334a991acf80cd45976896f98609
harzo <harzojunior@gmail.com>
2021-05-22 23:45:02 +0200

    chore: Lint and format

--- contracts/UltiToken.sol
+++ contracts/UltiToken.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPLv3
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol';
+import '@openzeppelin/contracts/security/Pausable.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 
 contract UltiToken is ERC20, ERC20Capped, ERC20Burnable, Pausable, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
 
     uint256 public constant INITIAL_SUPPLY = 40 * 1e9 * 1e18;
 
-    constructor() ERC20("UltiToken", "ULTI") ERC20Capped(150 * 1e9 * 1e18) {
+    constructor() ERC20('UltiToken', 'ULTI') ERC20Capped(150 * 1e9 * 1e18) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
         _setupRole(PAUSER_ROLE, msg.sender);
@@ -37,8 +37,11 @@ contract UltiToken is ERC20, ERC20Capped, ERC20Burnable, Pausable, AccessControl
         ERC20Capped._mint(account, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
