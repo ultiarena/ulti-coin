2d8c688554a2a8eb4e4e2cdfce3e08ce18e9d0b9
harzo <harzojunior@gmail.com>
2021-08-29 17:00:15 +0200

    feat: Update scripts

--- scripts/coin_bytecode.ts
+++ scripts/coin_bytecode.ts
@@ -1,16 +1,15 @@
 import { ethers } from 'hardhat'
 
 async function main() {
+  const owner = '0x8595c4Ad15D51c5Bf920c249869Ec5b3250c2D4d'
+  const pancakeRouterAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+  
   const tokenFactory = await ethers.getContractFactory('UltiCoin')
-
-  const owner = '0x'
-  // mainnet BSC Router Address: 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F
-  // testnet BSC Router Address: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
-  const pancakeRouterAddress = '0x'
   const deployTx = await tokenFactory.getDeployTransaction(owner, pancakeRouterAddress)
 
-  console.log('UltiCoin bytecode:')
-  console.log(deployTx.data)
+  console.log(`UltiCoin bytecode:\n${deployTx.data}`)
+  // @ts-ignore
+  console.log(`UltiCoin bytecode size: ${deployTx.data.length/2}`)
 }
 
 main()
--- scripts/coin_deploy.ts
+++ scripts/coin_deploy.ts
@@ -1,18 +1,27 @@
 import { ethers } from 'hardhat'
 
-async function main() {
+async function deploy(
+  owner: string,
+  pancakeRouterAddress: string
+) {
   const tokenFactory = await ethers.getContractFactory('UltiCoin')
+  const token = await tokenFactory.deploy(owner, pancakeRouterAddress, {
+    gasLimit: 10000000,
+  })
+  await token.deployed()
+  return token.address
+}
 
-  const owner = '0x'
-  // mainnet BSC Router Address: 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F
+
+async function main() {
+  // mainnet BSC Router Address: 0x10ED43C718714eb63d5aA57B78B54704E256024E
   // testnet BSC Router Address: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
   // kovan Router Address: 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
-  const pancakeRouterAddress = '0x'
-  const token = await tokenFactory.deploy(owner, pancakeRouterAddress, {
-    gasLimit: 7600000,
-  })
 
-  console.log('UltiCoin deployed to:', token.address)
+  const owner = '0x8595c4Ad15D51c5Bf920c249869Ec5b3250c2D4d'
+  const pancakeRouterAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+  const tokenAddress = await deploy(owner, pancakeRouterAddress)
+  console.log('UltiCoin deployed to:', tokenAddress)
 }
 
 main()
