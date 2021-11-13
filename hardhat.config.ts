141842da07a3de85200f1407af88dc79bda6966b
harzo <harzojunior@gmail.com>
2021-11-13 14:58:32 +0100

    feat: Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,56 @@
+import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
+import 'hardhat-abi-exporter'
+import 'hardhat-contract-sizer'
+import 'hardhat-gas-reporter'
+import 'hardhat-tracer'
 import 'solidity-coverage'
-const { mnemonic, etherscanApiKey, infuraApiKey } = require('./.secrets.json')
+import { HardhatUserConfig } from 'hardhat/config'
+const { privateKey, etherscanApiKey, infuraApiKey } = require('./.secrets.json')
 
-module.exports = {
+const config: HardhatUserConfig = {
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true
+  },
+  etherscan: {
+    apiKey: etherscanApiKey
+  },
+  gasReporter: {
+    coinmarketcap: process.env.CMC_API_KEY,
+    enabled: !!process.env.REPORT_GAS,
+    showTimeSpent: true
+  },
   networks: {
     kovan: {
       url: `https://kovan.infura.io/v3/${infuraApiKey}`,
       chainId: 42,
-      accounts: { mnemonic: mnemonic },
+      accounts: [privateKey],
     },
     bsc_testnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
       chainId: 97,
-      accounts: { mnemonic: mnemonic },
+      accounts: [privateKey],
     },
     bsc: {
       url: 'https://bsc-dataseed.binance.org/',
       chainId: 56,
-      accounts: { mnemonic: mnemonic },
+      accounts: [privateKey],
     },
     polygon_testnet: {
       url: `https://rpc-mumbai.maticvigil.com`,
       chainId: 80001,
-      accounts: { mnemonic: mnemonic },
+      accounts: [privateKey],
     },
     polygon: {
       url: 'https://rpc-mainnet.maticvigil.com',
       chainId: 137,
-      accounts: { mnemonic: mnemonic },
+      accounts: [privateKey],
     },
-  },
-  etherscan: {
-    apiKey: etherscanApiKey,
   },
   solidity: {
     compilers: [
@@ -45,14 +62,13 @@ module.exports = {
       },
     ],
     settings: {
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      optimizer: {
+        enabled: true
+      },
       metadata: {
         // do not include the metadata hash, since this is machine dependent
         // and we want all generated code to be deterministic
-        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        // https://docs.soliditylang.org/en/v0.8.7/metadata.html
         bytecodeHash: 'none',
       },
     },
@@ -64,3 +80,5 @@ module.exports = {
     artifacts: './artifacts',
   },
 }
+
+export default config
